a06145cb4ef69c5707d1467215ad1014705303c7
Rob <robmaskrey@gmail.com>
2017-08-22 23:41:05 -0400

    personalized text and added background image

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Rob Maskrey</title>
+    <meta content='BJ-AxCSPNC8koVmBDKh5DbCcxjXmAU56jBf3Hsxqbuw' name='google-site-verification' />
+    <link href='css/style.css' media='screen' rel='stylesheet' type='text/css' />
+    <link rel="shortcut icon" href="favicon.ico?v=1" type="image/x-icon">
+    <link rel="icon" href="favicon.ico?v=1" type="image/x-icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+  </head>
+  <body>
+    <div class="container">
+      @yield ('content')
+    </div>
+  </body>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,24 @@
+@extends ('layouts.master')
+
+@section ('content')
+  <body>
+    <div class='content'>
+      <h1>Rob Maskrey</h1>
+      <p>I lived in Maine.</p>
+      <p>I now live in Pennsylvania.</p>
+      <p>I used to build a lot of things for a living.</p>
+      <p>I read comic books and maintain a pretty big collection.</p>
+      <p>I watch a lot movies and run a weekly movie night</a>.</p>
+      <p>I have a beautiful <a href = "https://www.facebook.com/elimez" target="_blank">wife</a>, a pair of excitable cats, and I live above a backpacking store on the Appalacian Trail.</p>
+      <p>I write <a href = "https://github.com/Robmaskrey" target="_blank">code</a> and <a href = "http://www.twitter.com/ianenders" target="_blank">idiotic comments</a> about the things I see.</p>
+      <p>I sometimes write <a href = "http://blog.ianenders.com">longer idiotic comments</a> too.</p>
+      <p>I was a creative asset for a lot of people around the country</p>
+      <p>I am not anymore, but they're still cool.</p>
+      <p>I am working with some awesome people right now building a network of interesting web applications.</p>
+      <p>I am the CEO of <a href = "url placeholder">CodingHat Studios</a>, and we recently joined the <a href="placeholder url">Keystone Innovation Initiaive</a> family.</p>
+      <p>I will not take on a project that doesn't make me happy or whole.</p>
+      <p>I have a more detailed <a href="/resume.html">résumé</a> if you're into that sort of thing.</p>
+    </div>
+  </body>
+
+@endsection
